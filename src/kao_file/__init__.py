094002a16410d2280fb49e30425bac67c8c52e79
Chris Loew <cloew123@gmail.com>
2015-06-02 13:42:26 -0600

    Removed dead improts

--- src/kao_file/__init__.py
+++ src/kao_file/__init__.py
@@ -3,5 +3,3 @@ from .file_line import FileLine
 from .file_section import FileSection
 
 from .section_finder import SectionFinder
-from .detectors.section_start_detector import SectionStartDetector
-from .detectors.section_end_detector import SectionEndDetector
